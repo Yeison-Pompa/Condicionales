6c2d85310eaba38dda49f2db223550ae5767ac17
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-06-07 17:28:24 -0500

    tomando el valor de los input

--- assets/js/ejercicio3.js
+++ assets/js/ejercicio3.js
@@ -2,12 +2,13 @@ const ingresar = document.querySelector("#ingresar")
 
 ingresar.addEventListener("click", () =>{
 
+let numero_1 =parseInt(document.querySelector("#num_1").value)
+let numero_2 =parseInt(document.querySelector("#num_2").value)
+let numero_3 =parseInt(document.querySelector("#num_3").value)
 
 
 
 
 
 
-
-    
 })
