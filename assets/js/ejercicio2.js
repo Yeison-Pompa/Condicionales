5245ce57ad52f2230096460c439b37039d703abd
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-06-07 17:00:02 -0500

    capturando valores de los input por medio de variables

--- assets/js/ejercicio2.js
+++ assets/js/ejercicio2.js
@@ -1,1 +1,17 @@
 const verificar = document.querySelector("#verificar");
+
+verificar.addEventListener("click", () =>{
+
+let input1 = document.querySelector(".input1").value;
+let input2 = document.querySelector(".input2").value;
+let input3 = document.querySelector(".input3").value;
+
+input1 = Number (input1)
+input2 = Number (input2)
+input3 = Number (input3)
+
+
+
+
+
+})
