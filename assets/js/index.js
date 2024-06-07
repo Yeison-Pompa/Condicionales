127f17a7c6c945e192453daef33ccefaa9d368bf
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-06-07 16:33:41 -0500

    agregando la condicional

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
-const imagen = document.querySelector("img")
+const imagen = document.querySelector("img");
 
-imagen.addEventListener("click", () =>{
-
-
-
-
-    
-})
+imagen.addEventListener("click", () => {
+  if (imagen.style.border == "") {
+    imagen.style.border = "2px solid red";
+  } else {
+    imagen.style.border = "";
+  }
+});
