003f7dccfadf1d72dac6df7f6ae88c89fa188739
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-06-07 17:32:55 -0500

    agregando las condicionales

--- assets/js/ejercicio3.js
+++ assets/js/ejercicio3.js
@@ -1,14 +1,15 @@
-const ingresar = document.querySelector("#ingresar")
+const ingresar = document.querySelector("#ingresar");
 
-ingresar.addEventListener("click", () =>{
+ingresar.addEventListener("click", () => {
+  let numero_1 = parseInt(document.querySelector("#num_1").value);
+  let numero_2 = parseInt(document.querySelector("#num_2").value);
+  let numero_3 = parseInt(document.querySelector("#num_3").value);
 
-let numero_1 =parseInt(document.querySelector("#num_1").value)
-let numero_2 =parseInt(document.querySelector("#num_2").value)
-let numero_3 =parseInt(document.querySelector("#num_3").value)
-
-
-
-
-
-
-})
+  if (numero_1 === 9 && numero_2 === 1 && numero_3 === 1) {
+    document.querySelector("p").innerHTML = "contraseña 1 correcta";
+  } else if (numero_1 === 7 && numero_2 === 1 && numero_3 === 4) {
+    document.querySelector("p").innerHTML = "contraseña 2 correcta";
+  } else {
+    document.querySelector("p").innerHTML = "contraseña incorrecta";
+  }
+});
